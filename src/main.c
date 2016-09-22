80b31764ac657fd0870a581f8aae249513163302
logiclogue <jordanpaullord@hotmail.co.uk>
2016-09-22 22:56:15 +0100

    Removes comments

--- src/main.c
+++ src/main.c
@@ -8,18 +8,18 @@ int main()
 {
     Game *game = Game_main(8);
     Piece *piece = Piece_main('M', 'P');
-    //*game->board[7][7] = piece;
-    //
-    //printf("This: %c\n", game->board[7][7]->type);
-    //dialog_sample_game();
-    //dialog_play_again();
-    //game->start_square = dialog_get_piece_coords();
-    //game->finish_square = dialog_get_move_coords();
+    game->board[7][7] = piece;
+    
+    printf("This: %c\n", game->board[7][7]->type);
+    dialog_sample_game();
+    dialog_play_again();
+    game->start_square = dialog_get_piece_coords();
+    game->finish_square = dialog_get_move_coords();
 
-    //printf("%d, %d\n", game->start_square[0], game->start_square[1]);
-    //printf("%d, %d\n", game->finish_square[0], game->finish_square[1]);
+    printf("%d, %d\n", game->start_square[0], game->start_square[1]);
+    printf("%d, %d\n", game->finish_square[0], game->finish_square[1]);
 
-    //Game_make_move(game);
+    Game_make_move(game);
     
     piece->is_move_legal(piece, game);
 
