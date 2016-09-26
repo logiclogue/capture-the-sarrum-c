0d8b3dad62351f93d4b9dbf0c4190edf5f5f6d2e
logiclogue <jordanpaullord@hotmail.co.uk>
2016-09-26 15:39:20 +0100

    Updates sample game type to be the same as original

--- src/game_type.c
+++ src/game_type.c
@@ -38,12 +38,12 @@ void game_type_normal(Game *game)
 void game_type_sample(Game *game)
 {
     Board board = game->board;
-    board[0][1] = Piece_main('G', 'B');
-    board[0][3] = Piece_main('S', 'B');
-    board[0][7] = Piece_main('G', 'W');
-    board[1][0] = Piece_main('R', 'W');
-    board[2][0] = Piece_main('S', 'W');
-    board[2][1] = Piece_main('E', 'B');
-    board[2][7] = Piece_main('E', 'B');
-    board[5][7] = Piece_main('R', 'B');
+    board[1][0] = Piece_main('G', 'B');
+    board[3][0] = Piece_main('S', 'B');
+    board[7][0] = Piece_main('G', 'W');
+    board[0][1] = Piece_main('R', 'W');
+    board[0][2] = Piece_main('S', 'W');
+    board[1][2] = Piece_main('E', 'B');
+    board[7][2] = Piece_main('E', 'B');
+    board[7][5] = Piece_main('R', 'B');
 }
